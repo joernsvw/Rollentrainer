57bfcac1cd668898cb9ca9b26e0fbab6765166b8
joernsvw <joernboeschen1@gmail.com>
2026-09-24 16:02:46 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 // Holt immer zuerst die aktuelle Version aus dem Netz, offline die zuletzt gespeicherte.
-const CACHE = 'rolle-v1';
+const CACHE = 'rolle-v2';
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
 self.addEventListener('fetch', e => {
   if (e.request.method !== 'GET' || new URL(e.request.url).origin !== location.origin) return;
   e.respondWith(
-    fetch(e.request).then(res => {
+    fetch(e.request, { cache: 'no-cache' }).then(res => {
       const copy = res.clone();
       caches.open(CACHE).then(c => c.put(e.request, copy));
       return res;
